faad21769aeea1d268b49391b83534d1dcefd940
Jaganprabu <Jaganprabu.Dharmaiah@csscorp.com>
2022-03-10 13:56:46 +0530

    header images path modified

--- src/Pages/Header/index.tsx
+++ src/Pages/Header/index.tsx
@@ -19,21 +19,21 @@ const Header = () => {
         
           <div className='my-6 p-2.5'>
             <a href="#">
-              <img src="https://e.bobbibrown.na.us.stage.ncsa.elcdev.net/sites/bobbibrown/themes/bb_base/img/icons/svg/Search-White.svg"
+              <img src="https://www.bobbibrowncosmetics.com/sites/bobbibrown/themes/bb_base/img/icons/svg/Search-White.svg"
                 alt="search" className='h-5 w-5' />
             </a>
           </div>
 
           <div className='my-6 p-2.5'>
             <a href="#">
-              <img src="https://e.bobbibrown.na.us.stage.ncsa.elcdev.net/sites/bobbibrown/themes/bb_base/img/icons/svg/MyAccount-White.svg"
+              <img src="https://www.bobbibrowncosmetics.com/sites/bobbibrown/themes/bb_base/img/icons/svg/MyAccount-White.svg"
                 alt="My account" className='h-5 w-5'/>
             </a>
           </div>
 
           <div className='my-6 p-2.5'>
             <a href="#">
-              <img src="https://e.bobbibrown.na.us.stage.ncsa.elcdev.net/sites/bobbibrown/themes/bb_base/img/icons/svg/Bag-White.svg"
+              <img src="https://www.bobbibrowncosmetics.com/sites/bobbibrown/themes/bb_base/img/icons/svg/Bag-White.svg"
                 alt="Cart" className='h-5 w-5' />
             </a>
           </div>
